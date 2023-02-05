74839a4f18fb75276b121bcdca8318b941ef44db
VilarianBreaux <Vmb0157309@lpssonline.com>
2023-02-05 02:08:31 +0000

    Circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        var drawCircle = function() {
+            circle = draw.randomCircleInArea(canvas, true, false, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        for (var drawLoops = 0; drawLoops < 100; drawLoops++) {
+            drawCircle();
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -45,7 +52,11 @@ var init = function (window) {
            
 
             // TODO 9 : Iterate over the array
-           
+           for (var eachCircle = 0; eachCircle < circles.length; eachCircle++) {
+                var eachValue = Circle[eachCircle];
+                physikz.updatePosition(circles[eachCircle]);
+                game.checkCirclePositon(circles[0])
+           }
             
         }
     
@@ -62,9 +73,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+            else if( circle.x > canvas.width ) {
+                    circle.x =0;
+            }
+            else if (circle.y > canvas.height) {
+                circle.y = 0;
+            }
+            else if (circle. y < 0) {
+                circle.y = canvas.height
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
